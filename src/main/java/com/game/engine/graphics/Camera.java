a7852897e6a2f9cb077c1ab6693903f031fd7409
Andrei <andrei.doroshkov89@gmail.com>
2016-05-30 22:21:05 +0300

    fix motion camera

--- src/main/java/com/game/engine/graphics/Camera.java
+++ src/main/java/com/game/engine/graphics/Camera.java
@@ -6,6 +6,8 @@ public class Camera {
 
     private final Vector3f position;
     private final Vector3f rotation;
+    private double speed = 0.15d;
+
 
     public Camera() {
         position = new Vector3f(0, 0, 0);
@@ -18,15 +20,22 @@ public class Camera {
     }
 
     public void movePosition(float offsetX, float offsetY, float offsetZ) {
-        if ( offsetZ != 0 ) {
-            position.x += (float)Math.sin(Math.toRadians(rotation.y)) * -1.0f * offsetZ;
-            position.z += (float)Math.cos(Math.toRadians(rotation.y)) * offsetZ;
+        if (offsetZ < 0) {
+            position.x += Math.sin(rotation.y * Math.PI / 180) * speed;
+            position.z += -Math.cos(rotation.y * Math.PI / 180) * speed;
         }
-        if ( offsetX != 0) {
+        if (offsetZ > 0) {
+            position.x -= Math.sin(rotation.y * Math.PI / 180) * speed;
+            position.z -= -Math.cos(rotation.y * Math.PI / 180) * speed;
+        }
+        if (offsetX < 0) {
+            position.x += Math.sin((rotation.y - 90) * Math.PI / 180) * speed;
+            position.z += -Math.cos((rotation.y - 90) * Math.PI / 180) * speed;
+        }
+        if ( offsetX > 0) {
             position.x += (float)Math.sin(Math.toRadians(rotation.y - 90)) * -1.0f * offsetX;
             position.z += (float)Math.cos(Math.toRadians(rotation.y - 90)) * offsetX;
         }
-        position.y += offsetY;
     }
 
     public Vector3f getPosition() {
